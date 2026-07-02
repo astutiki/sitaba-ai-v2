44bddbf5e5cf7cb3e0a37cf3e4b179e31e9b7cb7
astutiki <as.tutikias91@gmail.com>
2026-07-02 19:14:50 +0700

    UPDATE main.py

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from api.chat import router as chat_router
-    app.include_router(chat_router)
-
 from api.auth import router as auth_router
-    app.include_router(auth_router)
-
 from api.debug import router as debug_router
-    app.include_router(debug_router)
 
 from config import (
     APP_TITLE,
@@ -29,6 +25,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(chat_router)
+app.include_router(auth_router)
+app.include_router(debug_router)
 
 
 @app.get("/")
